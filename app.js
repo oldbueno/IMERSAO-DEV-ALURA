0e9f9fe22d1cf3cfc7145248bebd9c74b9612a97
oldbueno <149400540+oldbueno@users.noreply.github.com>
2024-09-06 12:19:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+function pesquisar() {
+  // Essa função busca por dados e exibe os resultados em uma seção HTML.
+
+  // Seleciona a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  // se campoPesquisa for uma string sem nada
+  if (campoPesquisa == ""){
+    section.innerHTML = "Digite um nome de uma lenda"
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada dado e constrói o HTML para cada item
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+
+      // Cria um novo elemento
+      resultados += `
+      <div class="item-resultado">
+        <h2>
+          <a href="#" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais Informações</a>
+      </div>
+    `;
+    }
+  }
+
+// Se a pesquisa for diferente dos textos, informa nada encontrado
+  if (!resultados){
+    resultados = "<p>Nada encontrado</p>"
+  }
+
+  // Atualiza o conteúdo da seção com os resultados
+  section.innerHTML = resultados;
+
+}
---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo: "Geromel",
+        descricao: "Pedro Geromel é um dos maiores zagueiros da história do Grêmio e um dos principais jogadores brasileiros da atualidade. Com uma liderança nata e uma técnica impecável, Geromel se tornou um ídolo da torcida gremista",
+        link: "https://pt.wikipedia.org/wiki/Pedro_Geromel",
+        tags: "geromito zagueiro 3"
+    },
+    {
+        titulo: "Kannemann",
+        descricao: "Walter Kannemann, o 'Viking', é outro zagueiro fundamental para a defesa gremista. Sua força física e agilidade são características marcantes, além de sua paixão pelo clube. Kannemann é um dos líderes da equipe.",
+        link: "https://pt.wikipedia.org/wiki/Walter_Kannemann",
+        tags: "viking raçadudo 4"
+    },
+    {
+        titulo: "Luis Suárez",
+        descricao: "Luis Suárez, o Pistolero, deixou sua marca no Grêmio com sua habilidade e faro de gol. O uruguaio foi um dos principais responsáveis por levar o Grêmio à final da Libertadores em 2021. Sua passagem pelo Tricolor foi marcante.",
+        link: "https://pt.wikipedia.org/wiki/Luis_Alberto_Su%C3%A1rez",
+        tags: "el pistolero pistoleiro uruguaio"
+    },
+    {
+        titulo: "Renato Portaluppi",
+        descricao: "Conhecido como Renato Gaúcho, é uma lenda tanto como jogador quanto como técnico do Grêmio. Foi peça chave na conquista da Libertadores e do Mundial em 1983. Além de atualmente ser técnico do Grêmio e ter conquistados diversos titulos como treinador também.",
+        link: "https://pt.wikipedia.org/wiki/Renato_Ga%C3%BAcho",
+        tags: "gaucho"
+    },
+    {
+        titulo: "Paulo Nunes",
+        descricao: "Um atacante habilidoso, conhecido por sua velocidade e habilidade de marcar gols importantes, principalmente na década de 90.",
+        link: "https://pt.wikipedia.org/wiki/Paulo_Nunes",
+        tags: "diabo loiro"
+    },
+    {
+        titulo: "Everaldo",
+        descricao: "Lateral-esquerdo que se destacou no final dos anos 60 e início dos 70, sendo também campeão mundial com a seleção brasileira em 1970.",
+        link: "https://pt.wikipedia.org/wiki/Everaldo_Marques_da_Silva",
+        tags: "estrela dourada"
+    }
+    
+];
